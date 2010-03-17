4cdbc1d61d0cca7033bcbaa014816f35bc798754
pendergast.gary <pendergast.gary@4b7f0800-c249-11de-ad5a-915b6f3f5ed0>
2010-03-17 16:31:45 +0000

    TAG: 0.6.4

---
+++ tags/0.6.4/functions.php
@@ -0,0 +1,100 @@
+<?php
+function jobman_create_dashboard( $widths, $functions, $titles ) {
+?>
+<div id="dashboard-widgets-wrap">
+	<div id='dashboard-widgets' class='metabox-holder'>
+<?php
+	$ii = 0;
+	foreach( $widths as $width ) {
+?>
+		<div class='postbox-container' style='width:<?php echo $width ?>'>
+			<div id='normal-sortables' class='meta-box-sortables'>
+<?php
+		$jj = 0;
+		foreach( $functions[$ii] as $function ) {
+			jobman_create_widget( $function, $titles[$ii][$jj] );
+			$jj++;
+		}
+?>
+			</div>
+		</div>
+<?php
+		$ii++;
+	}
+?>
+	</div>
+	<div class="clear"></div>
+</div>
+<?php
+}
+
+function jobman_create_widget( $function, $title ) {
+?>
+				<div id="jobman-<?php echo $function ?>" class="postbox jobman-postbox">
+					<div class="handlediv" title="<?php _e('Click to toggle') ?>"><br /></div>
+					<h3 class='hndle'><span><?php echo $title ?></span></h3>
+					<div class="inside">
+<?php
+	call_user_func( $function );
+?>
+						<div class="clear"></div>
+					</div>
+				</div>
+<?php
+}
+
+function jobman_load_translation_file() {
+	load_plugin_textdomain( 'jobman', '', JOBMAN_FOLDER . '/translations' );
+}
+
+function jobman_page_taxonomy_setup() {
+	// Create our new page types
+	register_post_type( 'jobman_job', array( 'exclude_from_search' => false ) );
+	register_post_type( 'jobman_joblist', array( 'exclude_from_search' => true ) );
+	register_post_type( 'jobman_app_form', array( 'exclude_from_search' => true ) );
+	register_post_type( 'jobman_app', array( 'exclude_from_search' => true ) );
+	register_post_type( 'jobman_register', array( 'exclude_from_search' => true ) );
+	register_post_type( 'jobman_email', array( 'exclude_from_search' => true ) );
+
+	// Create our new taxonomy thing
+	$options = get_option( 'jobman_options' );
+	
+	$root = get_page( $options['main_page'] );
+	$url = get_page_uri( $root->ID );
+
+	register_taxonomy( 'jobman_category', array( 'jobman_job', 'jobman_app' ), array( 'hierarchical' => false, 'label' => __( 'Category', 'series' ), 'query_var' => 'jcat', 'rewrite' => array( 'slug' => $url ) ) );
+}
+
+function jobman_page_hierarchical_setup( $types ) {
+	$types[] = 'jobman_job';
+	$types[] = 'jobman_joblist';
+	$types[] = 'jobman_app_form';
+	$types[] = 'jobman_register';
+
+	return $types;
+}
+
+function jobman_sort_fields( $a, $b ) {
+	if($a['sortorder'] == $b['sortorder'])
+		return 0;
+	
+	return ( $a['sortorder'] < $b['sortorder'] ) ? -1 : 1;
+}
+
+function jobman_current_url() {
+		$pageURL = 'http';
+		
+		if( array_key_exists( 'HTTPS', $_SERVER ) && $_SERVER["HTTPS"] == "on" )
+			$pageURL .= "s";
+		
+		$pageURL .= "://";
+		
+		if( $_SERVER["SERVER_PORT"] != "80" )
+			$pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+		else
+			$pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+
+		return $pageURL;
+}
+
+?>
